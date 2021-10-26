8a58ad5097c8ec42a600cf06e7cbb523d141c71b
uiharuayako <2451602772@qq.com>
2021-10-27 02:03:57 +0800

    本地化插件，使其支持中文

--- WinTitle.cs
+++ WinTitle.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using Dalamud.Plugin;
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Logging;
@@ -18,8 +19,7 @@ namespace WinTitle
 
         public string Name => "Window Title Changer";
 
-        [PluginService]
-        private DalamudPluginInterface PluginInterface { get; set; }
+        [PluginService] private DalamudPluginInterface PluginInterface { get; set; }
         private CommandManager commandManager { get; init; }
 
         public WinTitle(CommandManager commandManager)
@@ -33,20 +33,69 @@ namespace WinTitle
             this.commandManager.AddHandler("/wintitle", new CommandInfo(WintitleCommand)
             {
                 ShowInHelp = true,
-                HelpMessage = "Change window title. Empty to revert.",
+                HelpMessage = "改变窗口标题",
             });
         }
 
+        static bool ContainChinese(string input)
+        {
+            string pattern = "[\u4e00-\u9fbb]";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        public static string GetHanNumFromString(string str)
+        {
+            int count = 0;
+            int addNum = 0;
+            string add = "";
+            Regex regex = new Regex(@"^[\u4E00-\u9FA5]{0,}$");
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (regex.IsMatch(str[i].ToString()))
+                {
+                    count++;
+                }
+            }
+
+            // 只要存在汉字，则执行
+            if (count > 0)
+            {
+                addNum = 2 * count - 1;
+            }
+
+            for (int i = 0; i < addNum; i++)
+            {
+                add = add + "1";
+            }
+
+            return add;
+        }
+
+        // user32dll的SetWindowText函数有问题
+        // 一个汉字会吞掉1个结尾字符
+        // 2个汉字吞掉3个
+        // 3个吞掉5个
+        // 4个吞掉7个
+        // 可知字符串中有n个汉字，会吞掉2n-1个字符
         public void SetTitle(string title)
         {
-            if (string.IsNullOrWhiteSpace(title)) title = this.OriginalTitle;
-            if (string.IsNullOrWhiteSpace(title)) title = "FINAL FANTASY XIV";
-            try { SetWindowText(this.Handle, title.Trim()); } catch (Exception ex) { PluginLog.LogError(ex, $"Failed to set Window Title to {title}"); }
+            //if (string.IsNullOrWhiteSpace(title)) title = this.OriginalTitle;
+            if (string.IsNullOrWhiteSpace(title)) title = "最终幻想XIV";
+            try
+            {
+                SetWindowText(this.Handle, title + GetHanNumFromString(title));
+            }
+            catch (Exception ex)
+            {
+                PluginLog.LogError(ex, $"不能将标题设置为{title}");
+            }
         }
 
         private void WintitleCommand(string _, string title) => this.SetTitle(title);
 
         public bool IsDisposed { get; private set; }
+
         public void Dispose()
         {
             if (this.IsDisposed) return;
@@ -58,6 +107,7 @@ namespace WinTitle
 
             GC.SuppressFinalize(this);
         }
+
         ~WinTitle() => this.Dispose();
     }
 }
